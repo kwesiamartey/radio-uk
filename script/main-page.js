50a3560f50243ca4fc5888ae9fc9f259d9ecc2f7
Kwesiamartey <kwesiamartey@yahoo.com>
2024-12-11 12:52:11 +0000

    Update main-page.js

--- script/main-page.js
+++ script/main-page.js
@@ -1,5 +1,5 @@
  let stationData = []; // Store stations here to avoid multiple fetches
-    let selectedCountryPath = localStorage.getItem('selectedCountryPath') || "italyRadioList.json";
+    let selectedCountryPath = localStorage.getItem('selectedCountryPath') || "ukRadioList.json";
 
     //let selectedCountryPath = '';
    const audio = new Audio();
@@ -14,7 +14,7 @@
     const stationListContent = document.getElementById('stationListContent');
     const countryList = document.getElementById('countryList');
 
-    const selectedCountryPathName = localStorage.getItem('selectedCountryPathName') || "Italy";
+    const selectedCountryPathName = localStorage.getItem('selectedCountryPathName') || "UK";
 
 
     async function loadStationPlaceholders() {
